341da6362addd9c948d419b0f5dcf72b3c21056f
nagamatsu <nagamatsu@sib.tv>
2020-05-26 15:31:12 +0900

    【js-sample】アナログ時計作成

---
+++ analogclock/assets/js/count.js
@@ -0,0 +1,23 @@
+'use strict';
+
+setInterval(() => {
+  const now = new Date();
+
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+
+  //各針角度
+  const handH = hour * (360 / 12) + minute * (360 / 12 / 60);
+  const handM = minute * (360 / 60);
+  const handS = second * (360 / 60);
+
+  //
+  const elmentH = document.querySelector('.lineHour');
+  const elmentM = document.querySelector('.lineMin');
+  const elmentS = document.querySelector('.lineSec');
+
+  elmentH.style.transform = `rotate(${handH}deg)`;
+  elmentM.style.transform = `rotate(${handM}deg)`;
+  elmentS.style.transform = `rotate(${handS}deg)`;
+}, 1000);
